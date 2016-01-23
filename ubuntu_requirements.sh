90977244e803d5f85e52945ef2d48acec399c94c
Smite Chow <xiaopengyou2no1@gmail.com>
2016-01-23 21:46:29 +0800

    :art: update pillow requirements

--- ubuntu_requirements.sh
+++ ubuntu_requirements.sh
@@ -3,3 +3,6 @@ sudo apt-get -y install fonts-wqy-zenhei
 
 # fake x server
 sudo apt-get -y install xvfb
+
+# pillow dependence
+sudo apt-get -y install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
