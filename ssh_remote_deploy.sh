592bf24fd385825eab0962f4771ad90be92efab1
Smite Chow <xiaopengyou2no1@gmail.com>
2016-01-24 00:51:42 +0800

    :bug: fix ci settings

--- ssh_remote_deploy.sh
+++ ssh_remote_deploy.sh
@@ -1,8 +1,8 @@
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
     echo "Deploy production..."
-    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_HOME/production/weibo_weixin_screen_shot/deploy_production.sh
+    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST cd $DEPLOY_HOME/production/weibo_weixin_screen_shot && ./deploy_production.sh
 fi
 if [[ $TRAVIS_BRANCH == 'develop' ]]; then
     echo "Deploy Staging..."
-    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_HOME/staging/weibo_weixin_screen_shot/deploy_staging.sh
+    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST cd $DEPLOY_HOME/staging/weibo_weixin_screen_shot && ./deploy_staging.sh
 fi
--- stop_django_runserver.sh
+++ stop_django_runserver.sh
@@ -2,4 +2,4 @@ if [ -z "$PORT" ]; then
     echo "ERROR: environment variable \$PORT not exists!"
     exit 1
 fi
-ps axf|grep runserver|grep ${PORT}|grep -v grep|awk '{print "kill -9 " $1}'|sh
+ps axf|grep runserver|grep ${PORT}|grep -v grep|awk '{print " " $1}'|xargs kill -9
