1aaaaf35455f947d168b3cfdb73e84bc724c41aa
Smite Chow <xiaopengyou2no1@gmail.com>
2016-01-23 09:21:29 +0800

    :art: add UT for generate user media image by random name

--- django_project/django_project/tests_utils.py
+++ django_project/django_project/tests_utils.py
@@ -14,3 +14,16 @@ class GenerateUserMediaPath(TestCase):
         file_path = os.path.join(settings.MEDIA_ROOT, file_path)
         self.assertTrue(os.path.exists(file_path))
         os.remove(file_path)
+
+    def test_generate_user_media_image_path_by_random_name(self):
+        file_path = generate_user_media_image_path(name='exists', prefix='unittest')
+        file_path = os.path.join(settings.MEDIA_ROOT, file_path)
+        self.assertTrue(os.path.exists(file_path))
+
+        file_path2 = generate_user_media_image_path(name='exists', prefix='unittest')
+        file_path2 = os.path.join(settings.MEDIA_ROOT, file_path2)
+        self.assertTrue(os.path.exists(file_path2))
+
+        self.assertNotEqual(file_path, file_path2)
+        os.remove(file_path)
+        os.remove(file_path2)
