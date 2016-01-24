da8003992e550e4d15f11fc1070955d06f082696
Smite Chow <xiaopengyou2no1@gmail.com>
2016-01-24 12:15:38 +0800

    :art: update ubuntu requirement

--- ubuntu_requirements.sh
+++ ubuntu_requirements.sh
@@ -4,5 +4,8 @@ sudo apt-get -y install fonts-wqy-zenhei
 # fake x server
 sudo apt-get -y install xvfb
 
+# chromium browser
+sudo apt-get -y install chromium-browser
+
 # pillow dependence
 sudo apt-get -y install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
