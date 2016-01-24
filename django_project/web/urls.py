ba0c14ced831d32d2ff84eddb9b7b1b591c01573
Smite Chow <xiaopengyou2no1@gmail.com>
2016-01-24 12:38:30 +0800

    :bug: fix url regex

--- django_project/web/urls.py
+++ django_project/web/urls.py
@@ -5,6 +5,6 @@ from web import views
 urlpatterns = [
     url(r'^weibo/capture/$',
         views.WeiboCaptureView.as_view(), name='weibo-capture'),
-    url(r'^weibo/capture/(?P<base64_media_path>\w+)/$',
+    url(r'^weibo/capture/(?P<base64_media_path>.+)/$',
         views.WeiboCaptureResultView.as_view(), name='weibo-capture-result'),
 ]
